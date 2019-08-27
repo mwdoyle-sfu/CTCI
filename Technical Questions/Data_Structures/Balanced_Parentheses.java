998cab7f6173f44085b2f92b2b3e3685178fa31c
mwdoyle-sfu <mwdoyle@sfu.ca>
2019-08-26 22:28:45 -0700

    Added to Technical Questions

---
+++ Technical Questions/Data_Structures/Balanced_Parentheses.java
@@ -0,0 +1,44 @@
+package Data_Structures;
+
+import java.util.Stack;
+
+public class Balanced_Parentheses {
+
+    public static char[][] TOKENS = {{'{', '}'},{'[', ']'},{'(', ')'}};
+
+    public static boolean isOpenTerm(char c) {
+        for (char[] array : TOKENS) {
+            if (array[0] == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean matches(char openTerm, char closeTerm) {
+        for (char[] array : TOKENS) {
+            if (array[0] == openTerm) {
+                return array[1] == closeTerm;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isBalanced(String expression) {
+    Stack<Character> stack = new Stack<Character>();
+        for (char c : expression.toCharArray()) {
+            if (isOpenTerm(c)){
+                stack.push(c);
+            } else {
+                if (stack.isEmpty() || !matches(stack.pop(), c)) {
+                    return false;
+                }
+            }
+        }
+        return  stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBalanced("{[]}"));
+    }
+}
---
+++ Technical Questions/Data_Structures/Linked_List.java
@@ -0,0 +1,47 @@
+package Data_Structures;
+import java.util.LinkedList;
+
+public class Linked_List {
+
+    public static void main(String[] args) {
+
+        // Create object of class linked list
+        LinkedList<String> object = new LinkedList<String>();
+
+        // Add elements to the linked list
+        object.add("A");
+        object.add("B");
+        object.addLast("C");
+        object.addFirst("D");
+        object.add(2, "E");
+        object.add("F");
+        object.add("G");
+        System.out.println("Linked list: " + object);
+
+        // Remove elements from the linked list
+        object.remove("B");
+        object.remove(3);
+        object.removeFirst();
+        object.removeLast();
+        System.out.println("Linked list after deletion: " + object);
+
+        // Finding elements in the linked list
+        boolean status = object.contains("E");
+
+        if (status) {
+            System.out.println("List contains the element 'E' ");
+        } else {
+            System.out.println("List doesnt contain the element 'E'");
+        }
+
+        // Number of elements in the linked list
+        int size = object.size();
+        System.out.println("Size of the linked list = " + size);
+
+        // Get and set elements from the linked list
+        Object element = object.get(2);
+        System.out.println("Element returned by get(): " + element);
+        object.set(2, "Y");
+        System.out.println("Linked list after change: " + object);
+    }
+}
